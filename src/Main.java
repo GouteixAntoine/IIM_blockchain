d3b0c6b36c86f5d7979dd3d760b077cdcb8510b3
GouteixAntoine <antoine.gouteix@edu.devinci.fr>
2020-01-25 19:10:49 +0100

    Adding main code to implement blockchain

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Création denotre blockchain avec une difficulté de 4 "Proof of Work"
+        Blockchain blockchain = new Blockchain(4);
+
+        // On ajoute des blocks avec des données que l'on veut sécuriser
+        blockchain.addBlock(blockchain.newBlock("Identifiant : antoinegouteix@orange.fr"));
+        blockchain.addBlock(blockchain.newBlock("Mot de passe : 5Tgdhs!khd579Jdjdjf"));
+        blockchain.addBlock(blockchain.newBlock("Nom et prénom : Gouteix Antoine"));
+
+        //System.out.println(blockchain);
+
+        // Ajout d'un block pour corrompre notre blockchain
+        //blockchain.addBlock(new Block(15, System.currentTimeMillis(), "Block corrompu", "Block invalide"));
+
+        // Affichage pour savoir si blockchain est valide ou pas ?
+        System.out.println("La blockchain est valide ou pas ? " + blockchain.isBlockChainValid());
+
+        // Visualiser la blockchain via sa représentation textuelle
+        System.out.println(blockchain);
     }
 }
